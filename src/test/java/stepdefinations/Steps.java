2ea9617da0ec3d44cb9de8194eec87c2ae4f0f99
saurabhj1414@gmail.com <saurabhj1414@gmail.com>
2026-07-10 14:45:55 +0530

    Cucumber Framework

---
+++ src/test/java/stepdefinations/Steps.java
@@ -0,0 +1,39 @@
+package stepdefinations;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+
+    @Given("the user is on nonprocommerce login page")
+    public void the_user_is_on_nonprocommerce_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user gives the valid username and password\\(username: {string}, password: {string})")
+    public void the_user_gives_the_valid_username_and_password_username_password(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicked on login button")
+    public void the_user_clicked_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user should be redirected to my account page")
+    public void the_user_should_be_redirected_to_my_account_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("user should be see the welcome msg")
+    public void user_should_be_see_the_welcome_msg() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
